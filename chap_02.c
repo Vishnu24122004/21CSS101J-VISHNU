1fcf1354daa83703afdc597816533971af471d50
Vishnu24122004 <113873698+Vishnu24122004@users.noreply.github.com>
2022-10-07 14:12:24 +0530

    Bio data

---
+++ chap_02.c
@@ -0,0 +1,4 @@
+int main()
+  printf("Vishnu");
+  printf("My department is computer science engineering with business system");
+  printf("I am studying in SRM Insitute of science and technology");
