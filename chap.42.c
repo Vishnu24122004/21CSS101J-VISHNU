eaabf7768c1eee5cadde7eba0b63e8a8240387a2
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-10-12 10:39:32 +0530

    To convert meter into feet

--- chap.42.c
+++ chap.42.c
@@ -3,12 +3,12 @@ int main() {
   float meter;
   float feet;
   
-	printfRA221042010043(" Program to convert Meter to Feet:  ");
+printfRA221042010043(" Program to convert Meter to Feet:  ");
 
-	printf("\n\n Enter Length in Meters  : ");
-	scanf("%f", &meter);
+printf("\n\n Enter Length in Meters  : ");	
+scanf("%f", &meter);
     
-    feet = (meter*3.26);
+feet = (meter*3.26);
     
-    printf("\n\n %f meter in feet = %f  \n",meter,feet);
+printf("\n\n %f meter in feet = %f  \n",meter,feet);
 }
