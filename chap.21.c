182354cf736733c8b500968fe61ffcb59a71ce7d
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-10-12 10:34:59 +0530

    convert meter into milimeter

--- chap.21.c
+++ chap.21.c
@@ -1,15 +1,9 @@
 int main()  
 {  
-    float  mm, m;  
-  
-    printfRA2211042010043("Enter distance in meter:");  
-    scanf("%f", &m);  
-  
-     
-    mm = m * 100.0;  
-  
-    
-    printf("Distance in Milimeter is %.2f\n", mm);  
-  
-    return 0;  
+float  mm, m;  
+printfRA2211042010043("Enter distance in meter:");  
+scanf("%f", &m);  
+mm = m * 100.0;  
+printf("Distance in Milimeter is %.2f\n", mm);  
+return 0;  
 }  
