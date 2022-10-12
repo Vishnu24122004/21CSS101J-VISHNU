5b266ed6466b7530e8e24f4ebb3d493922526140
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-10-12 10:37:14 +0530

    To find the Average number

--- chap.45.c
+++ chap.45.c
@@ -8,7 +8,7 @@ int main()
     printf("Enter second number: ");
     scanf("%d",&num2);
 
-    avg= (float)(num1+num2)/2;
+   avg= (float)(num1+num2)/2;
 
     printf("Average of %d and %d is= %.2f",num1,num2,avg);
 
