c706d82c3eda2e97da312339cb4fc1eaf0cd270d
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:01:07 +0530

    Assignment operator

--- 84.c
+++ 84.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
    int num1 = 10, num2 = 3, result; 
-   printf("RA2211042010057);
+   printf("RA2211042010043);
    result = num1 & num2; 
    printf("Bitwise AND result = %d\n", result);
     return 0;
