a742d7816600ac0594a2e3e89d47bfb4acd3d974
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 01:55:52 +0530

    Long int data type

---
+++ 81.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    long int num1 = 123456L, num2 = 123456L,result;
+    printf("RA2211042010057);
+    printf("Given integers are num1 = %d, num2 = %d\n", num1, num2);
+    result = num1 + num2;
+    printf("Sum of the given two number = %d\n",result);
+    return 0;
+}
