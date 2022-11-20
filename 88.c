ae906e77dc26e7ba3730f534cffa662e67d33429
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:08:26 +0530

    Character Oriented Functions

---
+++ 88.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("RA2211042010043");
+    printf("Enter a character : ");
+    ch = getchar();
+    printf("The entered character is : ");
+    putchar(ch);
+    return 0;
+}
