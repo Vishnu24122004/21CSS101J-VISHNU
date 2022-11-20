3641c8eb5c99c88a36444db0372c9668ae224867
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 01:59:34 +0530

    Comma operator

---
+++ 85.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   int num1, num2, result; 
+   printf("RA2211042010043);
+   result = (num1 = 6, num2 = num1 + 10, num1 +num2); 
+   printf("result = %d\n", result);
+    return 0;
+}
