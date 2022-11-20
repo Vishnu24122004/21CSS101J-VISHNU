3ba1120a96c56a0916e6261d74c44abb80a56b2d
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:06:10 +0530

    Getchar and putchar

---
+++ 86.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("RA2211042010043");
+    printf("Enter a character : ");
+    ch = getchar(); //use a function to read a character
+    printf("The entered character is : ");
+    putchar(ch);//use a function to print the character
+    return 0;
+}
