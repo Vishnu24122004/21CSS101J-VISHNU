e0d1c8173f8a5046b4c913c2df6d69dea0ecb717
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:15:04 +0530

    convert decimal integers to octal number

---
+++ 94.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int num;
+  printf("RA221104010043"\n)
+  printf("Enter a decimal integers : ");
+  scanf("%d", &num);
+  printf("Octal number : %o\n",num);
+  return 0;
+}
