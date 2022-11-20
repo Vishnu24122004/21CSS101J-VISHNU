6b0ad97279bdc710e0143d090468c1e23ebcb587
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:16:04 +0530

    convert decimal integers to hexadecimal

---
+++ 95.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int num;
+  printf("RA2211042010043"\n)
+  printf("Enter a decimal integers : ");
+  scanf("%d", &num);
+  printf("Hexa decimal number : %x\n",num);
+  return 0;
+}
