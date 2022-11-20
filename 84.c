6b904694c034d1791a21ed1ccd47de5511e38e41
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 01:58:40 +0530

    Bitwise AND operator

---
+++ 84.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   int num1 = 10, num2 = 3, result; 
+   printf("RA2211042010057);
+   result = num1 & num2; 
+   printf("Bitwise AND result = %d\n", result);
+    return 0;
+}
