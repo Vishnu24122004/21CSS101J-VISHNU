1df13ce4a694928e893ee8f50f7b22c4e8eda5e9
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:29:30 +0530

    To check the balence amount

---
+++ 105.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010043\n");
+    int balance,Sufficient_balance=1000;
+    printf("Enter balance : ");
+    scanf("%d",&balance);
+
+    if (balance>Sufficient_balance)
+        printf("Sufficient balance");
+    else 
+        printf("Low balance");
+    return 0;
+}
