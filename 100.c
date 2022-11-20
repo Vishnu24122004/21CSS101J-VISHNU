c9e6d3c50683a4813f215a8b7bec888440fda549
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:20:44 +0530

    Cobinations od c variable

---
+++ 100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010043\n");
+    printf("Result = %f\n", (2 + 2.5));
+    printf("Result = %d\n", ('A'+ 10));
+    return 0;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
