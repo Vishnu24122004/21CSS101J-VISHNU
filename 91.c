8c57d4b5c2c538d20ac853c9aebb0ea4a2cb26cb
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:11:09 +0530

    Bitwise OR operators

---
+++ 91.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1 = 10, num2 = 3, result; 
+    printf("RA2211042010043\n");
+    result = num1 | num2; 
+    printf("Bitwise OR result = %d\n", result);
+
+    return 0;
+}
