472ebe675a484bafe4f68575a0736e1ab2bc3486
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:27:48 +0530

    Ternary operator

---
+++ 104.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010043);
+    int marks = 75, pass_marks = 50;  (marks>pass_marks) ? printf("Passed-C-exam. \n"): printf("Failed C exam.\n");
+    return 0;
+}
