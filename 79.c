b007ec7b9fc9585f6a4e16302a95c5a6a4dc9039
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:02:14 +0530

    Exponential operator

--- 79.c
+++ 79.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     double num1 = 15.479, num2 = 25.7,result;
-    printf("RA2211042010057);
+    printf("RA2211042010043);
     printf("Given double value are num1 = %f, num2 = %f\n", num1, num2);
     result = num1 / num2;
     printf("Result of dividing = %d\n",result);
