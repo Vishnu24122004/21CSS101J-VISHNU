6b9806bbefc051e4d478bbfe104f6c6a99a87d9b
Vishnu,k <113873698+Vishnu24122004@users.noreply.github.com>
2022-11-21 02:12:08 +0530

    Bitwise XOR operators

---
+++ 92.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1 = 10, num2 = 5, result; 
+    printf("RA2211042010043\n");
+    result = num1 ^ num2; 
+    printf("Bitwise xOR result = %d\n", result);
+
+    return 0;
+}
